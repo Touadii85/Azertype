a81dc1d0c5ebbb2fea713aa1d6542058e2fc2a2d
Iliès Touadi <touadi.ilies@gmail.com>
2025-03-09 15:11:34 +0100

    update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 // Ecrivez votre code en dessous de ces commentaires.
 // Pensez à ouvrir l'onglet console pour consulter le résultat !
+/*
 totalLivre = 50
 totalLivre -=10
 totalLivre +=5
@@ -9,3 +10,37 @@ let affichageTotalLivres = "Notre bibliothèque possède "
 affichageTotalLivres += totalLivre
 affichageTotalLivres += " Livres."
 console.log(affichageTotalLivres)
+
+let ticket = {
+    nomFilm: "batman",
+    prix: 5,
+    numeroSalle: 8
+}
+console.log(ticket)
+
+let nom = "ilies"
+
+console.log(nom)
+
+let message = "Bonjour "+ nom + ", votre film " + ticket.nomFilm + " est en salle " + ticket.numeroSalle + "."
+console.log(message)
+*/
+let playlist = ["omega", "spiider-man", "disk war"]
+let totalMorceaux = playlist.length
+
+console.log(playlist)
+console.log(totalMorceaux)
+
+playlist.push('immundo')
+playlist.push('columbo')
+totalMorceaux = playlist.length
+
+console.log(playlist)
+console.log(totalMorceaux)
+
+playlist.pop()
+
+totalMorceaux = playlist.length
+
+console.log(playlist)
+console.log(totalMorceaux)
