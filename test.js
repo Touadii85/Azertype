eab1f11826990eaddb72740598d072c8ced8be62
Iliès Touadi <touadi.ilies@gmail.com>
2025-03-09 14:13:04 +0100

    initial commit

---
+++ test.js
@@ -0,0 +1,11 @@
+// Ecrivez votre code en dessous de ces commentaires.
+// Pensez à ouvrir l'onglet console pour consulter le résultat !
+totalLivre = 50
+totalLivre -=10
+totalLivre +=5
+console.log(totalLivre)
+
+let affichageTotalLivres = "Notre bibliothèque possède "
+affichageTotalLivres += totalLivre
+affichageTotalLivres += " Livres."
+console.log(affichageTotalLivres)
