4742d00c034162de946649b3c40f26e95a7c0b8b
Iliès Touadi <touadi.ilies@gmail.com>
2025-03-09 17:55:04 +0100

    commit scripts condition

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+/*
 console.log("Hello World");
+
+let motTapeOk = true // Essayez de mettre false à la place de true
+
+if (motTapeOk) {
+    console.log("Bravo, vous avez correctement tapé le mot")
+} else {
+    console.log("Échec, le mot n'est pas correct")
+}*/
+
+let painNoix = true
+let MaMonnaie = 8
+let prix = 6
+
+if(painNoix === true && MaMonnaie>=prix){
+    console.log("j\'achète du pain au noix")
+}else{
+    console.log("j\'achète une baguette")
+}
+
+let motUtilisateur = prompt("entrer un mot :")
+console.log(motUtilisateur)
