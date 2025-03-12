ed107ee4c67bf1629bb29c03465c0b9e17c4f5ad
Iliès Touadi <touadi.ilies@gmail.com>
2025-03-12 18:40:52 +0100

    Récupérez la valeur d'un champ de formulaire

--- AZERTYPE/scripts/script.js
+++ AZERTYPE/scripts/script.js
@@ -10,21 +10,26 @@
  * @param {number} nbMotsProposes : le nombre de mots proposés à l'utilisateur
  */
 function afficherResultat(score, nbMotsProposes) {
-    console.log("Votre score est de " + score + " sur " + nbMotsProposes)
+    let spanScore = document.querySelector(".zoneScore span")
+    
+    let affichageScore = `${score} / ${nbMotsProposes}`
+
+    spanScore.innerText = affichageScore
+    //console.log("Votre score est de " + score + " sur " + nbMotsProposes)
 }
 
 /**
  * Cette fonction demande à l'utilisateur de choisir entre "mots" et "phrases" et retourne le choix de l'utilisateur
  * @return {string} : le choix de l'utilisateur, ce choix est nécessairement "mots" ou "phrases
  */
-function choisirPhrasesOuMots() {
-    // Tant que l'utilisateur n'a pas saisi "mots" ou "phrases", on lui redemande de saisir un choix
-    let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
-    while (choix !== "mots" && choix !== "phrases") {
-        choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
-    }
-    return choix
-}
+// function choisirPhrasesOuMots() {
+//     // Tant que l'utilisateur n'a pas saisi "mots" ou "phrases", on lui redemande de saisir un choix
+//     let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+//     while (choix !== "mots" && choix !== "phrases") {
+//         choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+//     }
+//     return choix
+// }
 
 /**
  * Cette fonction lance la boucle de jeu, c'est à dire qu'elle demande à l'utilisateur de saisir tous les mots
@@ -33,18 +38,18 @@ function choisirPhrasesOuMots() {
  * @param {array[string]} listePropositions 
  * @return {number} : le score de l'utilisateur
  */
-function lancerBoucleDeJeu(listePropositions) {
-    let score = 0
-    for (let i = 0; i < listePropositions.length; i++) {
-        // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
-        let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
-        if (motUtilisateur === listePropositions[i]) {
-            // Si le mot saisi par l'utilisateur est correct, on incrémente le score
-            score++
-        }
-    }
-    return score
-}
+// function lancerBoucleDeJeu(listePropositions) {
+//     let score = 0
+//     for (let i = 0; i < listePropositions.length; i++) {
+//         // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
+//         let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
+//         if (motUtilisateur === listePropositions[i]) {
+//             // Si le mot saisi par l'utilisateur est correct, on incrémente le score
+//             score++
+//         }
+//     }
+//     return score
+// }
 
 /**
  * Cette fonction lance le jeu. 
@@ -52,18 +57,26 @@ function lancerBoucleDeJeu(listePropositions) {
  */
 function lancerJeu() {
     // Initialisations
-    let choix = choisirPhrasesOuMots()
+    // let choix = choisirPhrasesOuMots()
     let score = 0
     let nbMotsProposes = 0
+    
+    let btnValiderMot = document.getElementById("btnValiderMot")
+    let inputEcriture = document.getElementById("inputEcriture")
+
+    btnValiderMot.addEventListener("click", () =>{
+        console.log(inputEcriture.value)
+    })
+
 
     // On détermine la liste des mots ou des phrases à proposer à l'utilisateur
-    if (choix === "mots") {
-        score = lancerBoucleDeJeu(listeMots)
-        nbMotsProposes = listeMots.length
-    } else {
-        score = lancerBoucleDeJeu(listePhrases)
-        nbMotsProposes = listePhrases.length
-    }
+    // if (choix === "mots") {
+    //     score = lancerBoucleDeJeu(listeMots)
+    //     nbMotsProposes = listeMots.length
+    // } else {
+    //     score = lancerBoucleDeJeu(listePhrases)
+    //     nbMotsProposes = listePhrases.length
+    // }
 
     afficherResultat(score, nbMotsProposes)
 }
